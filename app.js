cb5fb1844b02bb84bde8483fb27c250549249631
Amber <Amberrmcclendon@gmail.com>
2021-07-20 09:58:58 -0400

    pushhw

---
+++ app.js
@@ -0,0 +1,31 @@
+var pants = [];
+for (var i = 1; i <= 10; i++) {
+  pants.push(i);
+  }
+  console.log (pants)
+
+
+
+
+// Only change code below this line
+var bananas = [];
+var i = 10;
+while(i >= 0) {
+  bananas.push(i);
+  i--;
+}
+console.log(bananas)
+
+var turtles = [];
+for(var i = 11; i <= 21; i+= 2){
+    turtles.push(i)
+}
+console.log(turtles)
+
+
+
+  var shirts = [];
+  for (var i = 15; i >= 1; i -= 2) {
+    shirts.push(i);
+  }
+  console.log(shirts)
